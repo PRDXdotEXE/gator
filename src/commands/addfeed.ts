13c7f75e4d70c56cba4cb3b6cc4b989e982dcf03
PRDXdotEXE <pratikpaudel77@outlook.com>
2026-09-06 09:43:24 +0000

    follwing feature added

--- src/commands/addfeed.ts
+++ src/commands/addfeed.ts
@@ -38,9 +38,6 @@ export async function handlerAddFeed(cmdName: string, ...args: string[]) {
 }
 
 function printFeed(feed: Feed, user: User) {
-    // console.log(`* ID:            ${feed.id}`);
-    // console.log(`* Created:       ${feed.createdAt}`);
-    // console.log(`* Updated:       ${feed.updatedAt}`);
     console.log(`* name:          ${feed.name}`);
     console.log(`* URL:           ${feed.url}`);
     console.log(`* User:          ${user.name}`);
